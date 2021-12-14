6b460e0203d14b572ea3a8b0a81a484adec2c1ca
NamithaChitrazee <namitha@pi.infn.it>
2021-12-14 15:26:50 +0100

    Update REveEventDisplay_module.cc

--- src/REveEventDisplay_module.cc
+++ src/REveEventDisplay_module.cc
@@ -183,6 +183,7 @@ namespace mu2e
     std::cout<<"*********** REve Mu2e **************"
     <<" User Options: "
     <<" addHits : "<<filler_.addHits_
+    <<" addTimeClusters : "<<filler_.addTimeClusters_
     <<" addClusters : "<<filler_.addClusters_
     <<" addTracks : "<<filler_.addKalSeeds_
     <<" addCosmicTrackSeeds : "<<filler_.addCosmicTrackSeeds_<<std::endl;
@@ -202,6 +203,7 @@ namespace mu2e
       std::cout<<"[REveEventDisplay : analyze()] -- Fill collections "<<std::endl;
       if(filler_.addClusters_)  filler_.FillRecoCollections(event, data, CaloClusters);
       if(filler_.addHits_)  filler_.FillRecoCollections(event, data, ComboHits);
+      if(_filler.addTimeClusters_)_filler.FillRecoCollections(event, data, TimeClusters);
       if(filler_.addKalSeeds_)  filler_.FillRecoCollections(event, data, KalSeeds);
       if(filler_.addCosmicTrackSeeds_)  filler_.FillRecoCollections(event, data, CosmicTrackSeeds);
       if(filler_.addMCTraj_)  filler_.FillMCCollections(event, data, MCTrajectories);
@@ -270,7 +272,7 @@ namespace mu2e
       REX::REveElement* scene = eveMng_->GetEventScene();
 
       std::cout<<"[REveEventDisplay : process_single_event] -- calls to data interface "<<std::endl;
-      DrawOptions drawOpts(false, filler_.addCosmicTrackSeeds_, filler_.addKalSeeds_, filler_.addClusters_, filler_.addHits_);
+      DrawOptions drawOpts(false, filler_.addCosmicTrackSeeds_, filler_.addKalSeeds_, filler_.addClusters_, filler_.addHits_, filler.addTimeClusters_);
       frame_->showEvents(eveMng_, scene, firstLoop_, data, drawOpts);
       
       std::cout<<"[REveEventDisplay : process_single_event] -- cluster added to scene "<<std::endl;
