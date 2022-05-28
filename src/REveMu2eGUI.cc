c254eda52ae82a26ed7d9482edb3eed412e94757
sophieMu2e <sophie@fnal.gov>
2022-05-28 14:28:02 -0500

    GUI

--- src/REveMu2eGUI.cc
+++ src/REveMu2eGUI.cc
@@ -5,9 +5,9 @@ using namespace mu2e;
 
 int REveMu2eGUI::WriteCoreJson(nlohmann::json &j, int rnr_offset)
 {
-  /*j["path"] = "data.root";
+  j["path"] = "data.root";
   j["count"] = fCount;
   j["total"] = fTotal;
-  j["UT_PostStream"] = "UT_refresh_event_info";*/
+  j["UT_PostStream"] = "UT_refresh_event_info";
   return ROOT::Experimental::REveElement::WriteCoreJson(j, 0);
 }
